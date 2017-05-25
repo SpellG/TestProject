17dff078a186db828c737cf7104570d71df8a3fe
SpellG <Gavin.Spellmeyer@Pfizer.com>
2017-05-25 13:29:23 -0400

    Test Project

---
+++ src/TestPackage/TestClass.java
@@ -0,0 +1,15 @@
+package TestPackage;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+	}
+
+}
